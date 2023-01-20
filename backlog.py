42fbcdaf95ad8a48910b5bc451857e5c1f8fff00
Mercala <63658397+Mercala@users.noreply.github.com>
2023-01-20 15:31:40 -0400

    Update backlog.py

--- backlog.py
+++ backlog.py
@@ -2,25 +2,25 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 def get_backlog_data(route: str, years: list, freq: str) -> pd.DataFrame:
-	  fp_back = 'data_backlog.csv'
-	  fp_deli = 'data_delivered.csv'
+	fp_back = 'data_backlog.csv'
+	fp_deli = 'data_delivered.csv'
 
-	  df_back = pd.read_csv(fp_back)
-	  df_deli = pd.read_csv(fp_deli)
+	df_back = pd.read_csv(fp_back)
+	df_deli = pd.read_csv(fp_deli)
 
-	  df_back.date = pd.to_datetime(df_back.date, format='%Y%m%d')
-	  df_back.set_index('date', inplace=True)
+	df_back.date = pd.to_datetime(df_back.date, format='%Y%m%d')
+	df_back.set_index('date', inplace=True)
 
-	  df_deli.date = pd.to_datetime(df_deli.date, format='%Y%m%d')
-	  df_deli.set_index('date', inplace=True)
+	df_deli.date = pd.to_datetime(df_deli.date, format='%Y%m%d')
+	df_deli.set_index('date', inplace=True)
 
-	  df_sample_back = df_back.resample(freq).sum()
-	  df_sample_deli = df_deli.resample(freq).sum()
+	df_sample_back = df_back.resample(freq).sum()
+	df_sample_deli = df_deli.resample(freq).sum()
 
-	  main_back = df_sample_back.drop('Unnamed: 8', axis=1)
-	  main_deli = df_sample_deli.drop('Unnamed: 8', axis=1)
+	main_back = df_sample_back.drop('Unnamed: 8', axis=1)
+	main_deli = df_sample_deli.drop('Unnamed: 8', axis=1)
 
-	  if freq == 'M':
+	if freq == 'M':
 		main_back.index = [date - timedelta(date.day - 1) for date in main_back.index]
 		main_deli.index = [date - timedelta(date.day - 1) for date in main_deli.index]
     
