84804419b347209f8c2f477690e75c6d4eb9ed96
Mercala <63658397+Mercala@users.noreply.github.com>
2023-01-20 09:37:38 -0400

    Update display.py

--- display.py
+++ display.py
@@ -1,50 +1,64 @@
 import streamlit as st
-import graph_sales
+from datetime import datetime
+
+import graph_total_volume
 import graph_backlog
-import gross_margin
-import sales
+import graph_margin
 
-PATH = ''
+legend = st.sidebar.selectbox('Legend', options=[False, True])
+mode = st.sidebar.selectbox('Barmode', options=['group', 'relative'])
+freq = st.sidebar.selectbox('Frequency', options=['M', 'W'])
+unit = st.sidebar.selectbox('Unit', options=['pounds', 'liters', 'gallons', 'kilos'])
 
-# # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-st.sidebar.subheader('LPG Sales Volume')
-toggle = st.sidebar.radio('Purchase', ('ON', 'OFF'))
-sales_fig = graph_sales.main(toggle)
+years = st.sidebar.multiselect('Years', options=['2021', '2022', '2023'], default='2022')
+
+route = st.sidebar.selectbox('Route', options=['all', '1', '2', '3A', '3B', '4', '5', '6'])
+currency = st.sidebar.selectbox('Currency', options=['awg', 'usd'])
+skew = st.sidebar.selectbox(
+	'SKU', options=[
+  		'HSH 100 lbs', 
+		'HSH 60 lb',
+		'HSH BULK',
+		'COM 20 lb',
+		'COM 60 lb',
+		'COM 100 lb',
+		'COM MIX',
+		'COM BULK',
+		'MOTOR FUEL'
+	]
+)
 
 st.title('Arugas Financial Dashboard')
 
+fig_volume = graph_total_volume.main(unit, years, freq, legend, mode)
 st.plotly_chart(
-	sales_fig,
-  	theme='streamlit',
-  	use_container_width=True,
-  	kwargs={'toggle': toggle}
-  	)
-# # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-st.sidebar.subheader('Backlog')
-year = st.sidebar.selectbox('Year', options=['2021', '2022'])
-backlog_fig = graph_backlog.main(year)
+	fig_volume,
+	kwargs={
+		'unit': unit,
+		'freq': freq,
+		'legend': legend,
+		'mode': mode
+	}
+)
 
+fig_backlog = graph_backlog.main(route, years, freq, legend)
 st.plotly_chart(
-	backlog_fig,
-	theme='streamlit',
-	use_container_width=True,
-	kwargs={'year': year}
-	)
-# # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-st.sidebar.subheader('Gross Margin')
-skew = st.sidebar.selectbox('SKU', options=['HSH 100 lb', 'HSH 60 lb', 'HSH BULK', 'COM 20 lb', 'COM 60 lb', 'COM 100 lb', 'COM MIX', 'COM BULK', 'MOTOR FUEL'])
-currency = st.sidebar.radio('Currency', ('awg', 'usd'))
-unit = st.sidebar.selectbox('Quantity unit', options=['kilos', 'pounds', 'gallons', 'liters'])
+	fig_backlog, 
+	kwargs={
+		'years': years,
+		'route': route,
+		'freq': freq,
+		'legend': legend
+	}
+)
 
-gross_margin_fig = gross_margin.graph(skew, currency, unit)
-
+fig_margin = gross_margin.main(skew, unit, currency, legend)
 st.plotly_chart(
-	gross_margin_fig,
-	theme='streamlit',
-	use_container_width=True,
+	fig_margin,
 	kwargs={
 		'skew': skew,
+		'unit': unit,
 		'currency': currency,
-		'unit': unit
-		}
-	)
+		'legend': legend
+	}
+)
