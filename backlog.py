874840b878ff4281f45c02bf5df10bed086a2cd1
Mercala <63658397+Mercala@users.noreply.github.com>
2023-01-31 08:36:27 -0400

    Update backlog.py
    Remove freq as arg

--- backlog.py
+++ backlog.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-def get_backlog_data(route: str, years: list, freq: str) -> pd.DataFrame:
+def get_backlog_data(route: str, years: list) -> pd.DataFrame:
 	fp_back = 'data_backlog.csv'
 	fp_deli = 'data_delivered.csv'
 
@@ -14,15 +14,16 @@ def get_backlog_data(route: str, years: list, freq: str) -> pd.DataFrame:
 	df_deli.date = pd.to_datetime(df_deli.date, format='%Y%m%d')
 	df_deli.set_index('date', inplace=True)
 
+	freq = 'W'
 	df_sample_back = df_back.resample(freq).sum()
 	df_sample_deli = df_deli.resample(freq).sum()
 
 	main_back = df_sample_back #.drop('Unnamed: 8', axis=1)
 	main_deli = df_sample_deli #.drop('Unnamed: 8', axis=1)
 
-	if freq == 'M':
-		main_back.index = [date - timedelta(date.day - 1) for date in main_back.index]
-		main_deli.index = [date - timedelta(date.day - 1) for date in main_deli.index]
+# 	if freq == 'M':
+# 		main_back.index = [date - timedelta(date.day - 1) for date in main_back.index]
+# 		main_deli.index = [date - timedelta(date.day - 1) for date in main_deli.index]
     
 	main_back.index.name = 'Date'
 	main_deli.index.name = 'Date'
